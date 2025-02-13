4876f3608ea64d20093e1a89ee1649f3616fba17
md-saifuzzaman-tonoy <saifuzzaman.tonoy@gmail.com>
2025-02-13 18:20:05 +0100

    aws s3 static website using terraform v1

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-north-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  default     = "my-static-website-bucket-123452055"
+}
